bb9d43378f16699e75ef1c1ee07865057a6fd58b
whatf0xx <hfox022@gmail.com>
2024-10-16 14:53:59 +0200

    100 collisions is a good amount to get a repeatable time interval

--- hundred_collisions.py
+++ hundred_collisions.py
@@ -2,6 +2,7 @@ from pickle import dump
 from eight_ball.ball import Ball
 from eight_ball.simulation import Simulation
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 if __name__ == "__main__":
     sim = Simulation(1.0)
@@ -13,10 +14,11 @@ if __name__ == "__main__":
     sim.add_balls(balls)
     sim.initialise()
     # sim.comic_strip(4)
-    for _ in range(100_000):
+    print("Running to thermodynamic equilibrium...")
+    for _ in tqdm(range(100_000)):
         sim.next_collision()
-    times_dist = sim.collision_times(1_000_000, 0., 0.02, 600)
-    with open("data/low_density_collision_times.pkl", "wb+") as f:
+    times_dist = sim.nth_collision_times(100, 5_00_000, 0., 1., 500)
+    with open("data/hundred_collision_times.pkl", "wb+") as f:
         dump(times_dist, f)
     fig = plt.figure()
     ax = plt.axes()
